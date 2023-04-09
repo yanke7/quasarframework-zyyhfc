8fcadab6a551a941d04dcb42de40fb46547300bf
yanke7 <kris@yankedigital.com>
2023-04-09 13:58:31 -0600

    1 file modified

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,110 +2,73 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-  },
-
-  //Customer Routes
-  {
-    path: '/customers/add',
-    component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Customers/CustomersAdd.vue') },
-    ],
-  },
-
-  {
-    path: '/customers/all',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Customers/CustomersAll.vue') },
-    ],
-  },
-
-  {
-    path: '/customers/report',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Customers/CustomersReport.vue') },
-    ],
-  },
-
-  {
-    path: '/customers/edit',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Customers/CustomersEdit.vue') },
-    ],
-  },
-
-  //Product Routes
-  {
-    path: '/products/add',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Products/ProductsAdd.vue') },
-    ],
-  },
-
-  {
-    path: '/products/edit',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Products/ProductsEdit.vue') },
-    ],
-  },
-
-  {
-    path: '/products/all',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Products/ProductsAll.vue') },
-    ],
-  },
-
-  {
-    path: '/products/report',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Products/ProductsReport.vue') },
-    ],
-  },
-
-  //Supplier Routes
-  {
-    path: '/suppliers/add',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Suppliers/SuppliersAdd.vue') },
-    ],
-  },
-
-  {
-    path: '/suppliers/all',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Suppliers/SuppliersAll.vue') },
-    ],
-  },
-
-  {
-    path: '/suppliers/edit',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
       {
-        path: '',
+        path: '/customers/add',
+        component: () => import('pages/Customers/CustomersAdd.vue'),
+      },
+      {
+        path: '/customers/all',
+        component: () => import('pages/Customers/CustomersAll.vue'),
+      },
+      {
+        path: '/customers/report',
+        component: () => import('pages/Customers/CustomersReport.vue'),
+      },
+      {
+        path: '/customers/edit',
+        component: () => import('pages/Customers/CustomersEdit.vue'),
+      },
+      {
+        path: '/products/add',
+        component: () => import('pages/Products/ProductsAdd.vue'),
+      },
+      {
+        path: '/products/all',
+        component: () => import('pages/Products/ProductsAll.vue'),
+      },
+      {
+        path: '/products/edit',
+        component: () => import('pages/Products/ProductsEdit.vue'),
+      },
+      {
+        path: '/products/setup',
+        component: () => import('pages/Products/ProductsSetup.vue'),
+      },
+      {
+        path: '/products/reports',
+        component: () => import('pages/Products/ProductsReports.vue'),
+      },
+      {
+        path: '/suppliers/add',
+        component: () => import('pages/Suppliers/SuppliersAdd.vue'),
+      },
+      {
+        path: '/suppliers/all',
+        component: () => import('pages/Suppliers/SuppliersAll.vue'),
+      },
+      {
+        path: '/suppliers/edit',
         component: () => import('pages/Suppliers/SuppliersEdit.vue'),
       },
-    ],
-  },
-
-  {
-    path: '/suppliers/report',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
       {
-        path: '',
-        component: () => import('pages/Suppliers/SuppliersReport.vue'),
+        path: '/suppliers/setup',
+        component: () => import('pages/suppliers/SuppliersSetup.vue'),
+      },
+      {
+        path: '/suppliers/reports',
+        component: () => import('pages/suppliers/SuppliersReports.vue'),
+      },
+      {
+        path: '/maps/maps',
+        component: () => import('pages/Maps/Maps.vue'),
+      },      {
+        path: '/maps/markermap',
+        component: () => import('pages/Maps/MarkerMap.vue'),
+      },      {
+        path: '/maps/streetview',
+        component: () => import('pages/Maps/Streetview.vue'),
       },
     ],
   },
